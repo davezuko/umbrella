c0c1ed0638a2ae4ba7e1c3c9d1e7086c20c97dac
David Zukowski <david@zuko.me>
2022-06-29 19:03:58 -0500

    Setup build script for figma packages

--- scripts/build.js
+++ scripts/build.js
@@ -28,41 +28,81 @@ let buildPackage = async (cwd) => {
         fs.readFileSync(path.join(cwd, "package.json"), "utf8"),
     )
 
-    if (!pkg.main || !/\.(ts|tsx)$/.test(pkg.main)) {
-        console.info(" [skip] %s", pkg.name)
-        return
-    }
-
-    console.info("[build] %s", pkg.name)
     let outdir = path.join(cwd, "dist")
     await fs.promises.rm(outdir, {
         recursive: true,
         force: true,
     })
 
+    if (pkg.name.includes("figma")) {
+        console.info("[build] %s", pkg.name)
+        return buildFigmaPackage(cwd)
+    }
+    if (!pkg.main || !/\.(ts|tsx)$/.test(pkg.main)) {
+        console.info(" [skip] %s", pkg.name)
+        return
+    }
+
+    console.info("[build] %s", pkg.name)
     let external = Object.keys(pkg.dependencies || {})
     fs.rmSync(outdir, {recursive: true, force: true})
-    let result = esbuild.buildSync({
-        entryPoints: [path.join(cwd, pkg.main)],
-        outfile: path.join(cwd, pkg.publishConfig.exports),
-        bundle: true,
-        platform: "node",
-        format: "esm",
-        external,
-        metafile: true,
-    })
+    let [result] = await Promise.all([
+        esbuild.build({
+            entryPoints: [path.join(cwd, pkg.main)],
+            outfile: path.join(cwd, pkg.publishConfig.exports),
+            bundle: true,
+            platform: "node",
+            format: "esm",
+            external,
+            metafile: true,
+        }),
+        esbuild.build({
+            entryPoints: [path.join(cwd, pkg.main)],
+            outfile: path.join(cwd, pkg.publishConfig.main),
+            bundle: true,
+            platform: "node",
+            format: "cjs",
+            external,
+        }),
+    ])
     if (verbose) {
         let text = await esbuild.analyzeMetafile(result.metafile)
         console.log(text)
     }
-    esbuild.buildSync({
-        entryPoints: [path.join(cwd, pkg.main)],
-        outfile: path.join(cwd, pkg.publishConfig.main),
-        bundle: true,
-        platform: "node",
-        format: "cjs",
-        external,
+}
+
+let buildFigmaPackage = async (cwd) => {
+    let outdir = path.join(cwd, "dist")
+    fs.rmSync(outdir, {recursive: true, force: true})
+    await Promise.all([
+        esbuild.build({
+            entryPoints: [
+                path.join(cwd, "plugin/plugin.tsx"),
+                path.join(cwd, "ui/ui.tsx"),
+            ],
+            outdir,
+            bundle: true,
+            splitting: false,
+            minify: true,
+            platform: "node",
+            format: "esm",
+            define: {
+                "process.env.NODE_ENV": JSON.stringify("production"),
+            },
+        }),
+    ])
+
+    let [ui, html] = await Promise.all([
+        fs.promises.readFile(path.join(cwd, "dist/ui/ui.js"), "utf8"),
+        fs.promises.readFile(path.join(cwd, "ui/ui.html"), "utf8"),
+    ])
+    html = html.replace("<script></script>", () => {
+        return `<script>\n\t\t\t${ui}\t\t</script>`
     })
+    await Promise.all([
+        fs.promises.rm(path.join(cwd, "dist/ui/ui.js")),
+        fs.promises.writeFile(path.join(cwd, "dist/ui/ui.html"), html, "utf8"),
+    ])
 }
 
 const cwd = process.cwd()
